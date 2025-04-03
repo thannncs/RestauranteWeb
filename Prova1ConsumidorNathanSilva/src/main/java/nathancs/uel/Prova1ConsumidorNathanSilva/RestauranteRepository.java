60618537d3ba2044791f2012d6b2c253893d3437
Nathan Costa Silva <102713233+thannncs@users.noreply.github.com>
2025-04-03 14:20:54 -0300

    Delete Prova1ConsumidorNathanSilva directory

--- Prova1ConsumidorNathanSilva/src/main/java/nathancs/uel/Prova1ConsumidorNathanSilva/RestauranteRepository.java
+++
@@ -1,5 +0,0 @@
-package nathancs.uel.Prova1ConsumidorNathanSilva;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RestauranteRepository extends CrudRepository<Restaurante,Integer> {
-}
