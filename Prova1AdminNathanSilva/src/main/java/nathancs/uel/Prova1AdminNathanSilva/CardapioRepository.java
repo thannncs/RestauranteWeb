f5d7d91b0a3a5876af0756929321f1ea8302d280
Nathan Costa Silva <102713233+thannncs@users.noreply.github.com>
2025-04-03 14:20:43 -0300

    Delete Prova1AdminNathanSilva directory

--- Prova1AdminNathanSilva/src/main/java/nathancs/uel/Prova1AdminNathanSilva/CardapioRepository.java
+++
@@ -1,8 +0,0 @@
-package nathancs.uel.Prova1AdminNathanSilva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface CardapioRepository extends JpaRepository<Cardapio,Integer> {
-    List<Cardapio> findByRestauranteId(int idRestaurante);
-}
