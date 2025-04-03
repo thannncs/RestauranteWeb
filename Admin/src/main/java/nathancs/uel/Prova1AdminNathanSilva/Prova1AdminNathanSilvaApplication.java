de2f76de264dd28854f3cdcca57316b5a02b4eb3
thanncs <thannn.cs@gmail.com>
2025-04-03 14:18:01 -0300

    Arrumando nome Cliente

---
+++ Admin/src/main/java/nathancs/uel/Prova1AdminNathanSilva/Prova1AdminNathanSilvaApplication.java
@@ -0,0 +1,13 @@
+package nathancs.uel.Prova1AdminNathanSilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova1AdminNathanSilvaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova1AdminNathanSilvaApplication.class, args);
+	}
+
+}
---
+++ Admin/src/test/java/nathancs/uel/Prova1AdminNathanSilva/Prova1AdminNathanSilvaApplicationTests.java
@@ -0,0 +1,13 @@
+package nathancs.uel.Prova1AdminNathanSilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova1AdminNathanSilvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
